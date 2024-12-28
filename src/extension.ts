d5d826bef36aecdd9bff634c50335d18bf3258bf
Jay <jordanbell321@gmail.com>
2024-12-28 21:46:11 +0000

    updated api save state to save usernames

--- src/extension.ts
+++ src/extension.ts
@@ -1,159 +1,219 @@
 import * as vscode from "vscode";
-import { startSession, endSession, getLanguageDurations } from "./utils/timeTracker";
-import { sendSessionData, checkAndValidateUserId } from "./utils/api";
+import {
+  startSession,
+  endSession,
+  getLanguageDurations,
+  getStreakData
+} from "./utils/timeTracker";
+import { sendSessionData, checkAndValidateUserId, getDiscordUsername } from "./utils/api";
 import { startRichPresence, stopRichPresence } from "./utils/rpcDiscord";
-import { getStreakData } from './utils/timeTracker';
 
 let extensionContext: vscode.ExtensionContext;
 let sessionStartTime: Date | null = null;
 let sessionTimerInterval: NodeJS.Timeout | null = null;
-const statusBarTimer = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+const statusBarTimer = vscode.window.createStatusBarItem(
+  vscode.StatusBarAlignment.Left,
+  100
+);
 
 export async function activate(context: vscode.ExtensionContext) {
-    console.log("<< Activating extension... >>");
-    extensionContext = context;
+  console.log("<< Activating extension... >>");
+  extensionContext = context;
 
-    // Automatically start tracking on load if enabled
-    sessionStartTime = new Date();
-    startSession();
+  // Create a status bar button for linking Discord
+  const statusBar = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    100
+  );
 
-    const enableRichPresence = vscode.workspace.getConfiguration("extension").get<boolean>("enableRichPresence");
-    if (enableRichPresence) {
-        startRichPresence();
-    }
+  let discordId = context.globalState.get<string>("discordId");
 
-    startSessionTimer();
-
-    // Create a status bar button for linking Discord
-    const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-    let discordId = context.globalState.get<string>('discordId');
-    console.log(`<< Discord User ID: ${discordId} >>`);
-
-    statusBar.text = discordId ? "Connected to Discord" : "Link to Discord";
-
-    // Set the command to allow re-linking Discord, even if already connected
-    statusBar.command = "extension.updateDiscordId";
-    statusBar.tooltip = "Click to update your Discord User ID";
-    statusBar.show();
-    context.subscriptions.push(statusBar);
-
-    // Register the command to link or update the Discord ID
-    context.subscriptions.push(
-        vscode.commands.registerCommand("extension.updateDiscordId", async () => {
-            const enteredDiscordId = await vscode.window.showInputBox({
-                prompt: "Enter your Discord User ID to link it with VSCode",
-                placeHolder: "Enter Discord User ID here",
-                value: discordId ?? "", // Show the current ID if available
-            });
-
-            // Check if entered ID is valid
-            if (enteredDiscordId && /^\d+$/.test(enteredDiscordId)) {
-                const isValid = await checkAndValidateUserId(enteredDiscordId);
-                if (isValid) {
-                    vscode.window.showInformationMessage("<< Discord ID is valid and exists! >>");
-                    await context.globalState.update('discordId', enteredDiscordId);
-                    vscode.window.showInformationMessage("<< Discord User ID updated successfully! >>");
-                    statusBar.text = "Connected to Discord";
-                    console.log(`<< Discord User ID updated to: ${enteredDiscordId} >>`);
-                }
-            } else if (enteredDiscordId) {
-                vscode.window.showErrorMessage("<< Invalid Discord ID | Enable Developer Mode In Discord And Try Again >>");
-            } else {
-                vscode.window.showErrorMessage("<< Discord ID is required to link your account >>");
-            }
-        })
+  if (!discordId) {
+    vscode.window.showErrorMessage(
+      "<< Discord ID is required to track sessions. Please link your Discord account. >>"
     );
+    return;
+  }
 
-    // Update Rich Presence based on configuration changes
-    vscode.workspace.onDidChangeConfiguration((e) => {
-        if (e.affectsConfiguration("extension.enableRichPresence")) {
-            const enableRichPresence = vscode.workspace.getConfiguration("extension").get<boolean>("enableRichPresence");
-            enableRichPresence ? startRichPresence() : stopRichPresence();
+  console.log(`<< Discord User ID: ${discordId} >>`);
+
+  // Automatically start tracking on load if enabled
+  sessionStartTime = new Date();
+  startSession();
+
+  const enableRichPresence = vscode.workspace
+    .getConfiguration("extension")
+    .get<boolean>("enableRichPresence");
+  if (enableRichPresence) {
+    startRichPresence();
+  }
+
+  startSessionTimer();
+
+  statusBar.text = discordId ? "Connected to Discord" : "Link to Discord";
+
+  // Set the command to allow re-linking Discord, even if already connected
+  statusBar.command = "extension.updateDiscordId";
+  statusBar.tooltip = "Click to update your Discord User ID";
+  statusBar.show();
+  context.subscriptions.push(statusBar);
+
+  // Register the command to link or update the Discord ID
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.updateDiscordId", async () => {
+      const enteredDiscordId = await vscode.window.showInputBox({
+        prompt: "Enter your Discord User ID to link it with VSCode",
+        placeHolder: "Enter Discord User ID here",
+        value: discordId ?? "", // Show the current ID if available
+      });
+
+      // Check if entered ID is valid
+      if (enteredDiscordId && /^\d+$/.test(enteredDiscordId)) {
+        const isValid = await checkAndValidateUserId(enteredDiscordId);
+        if (isValid) {
+          vscode.window.showInformationMessage(
+            "<< Discord ID is valid and exists! >>"
+          );
+          await context.globalState.update("discordId", enteredDiscordId);
+          vscode.window.showInformationMessage(
+            "<< Discord User ID updated successfully! >>"
+          );
+          statusBar.text = "Connected to Discord";
+          console.log(`<< Discord User ID updated to: ${enteredDiscordId} >>`);
         }
-    });
+      } else if (enteredDiscordId) {
+        vscode.window.showErrorMessage(
+          "<< Invalid Discord ID | Enable Developer Mode In Discord And Try Again >>"
+        );
+      } else {
+        vscode.window.showErrorMessage(
+          "<< Discord ID is required to link your account >>"
+        );
+      }
+    })
+  );
 
-    // Apply settings from configuration
-    applyConfigurationSettings();
+  // Update Rich Presence based on configuration changes
+  vscode.workspace.onDidChangeConfiguration((e) => {
+    if (e.affectsConfiguration("extension.enableRichPresence")) {
+      const enableRichPresence = vscode.workspace
+        .getConfiguration("extension")
+        .get<boolean>("enableRichPresence");
+      enableRichPresence ? startRichPresence() : stopRichPresence();
+    }
+  });
+
+  // Apply settings from configuration
+  applyConfigurationSettings();
 }
 
 function applyConfigurationSettings() {
-    const config = vscode.workspace.getConfiguration("extension");
+  const config = vscode.workspace.getConfiguration("extension");
 
-    const discordId = config.get<string>("updateDiscordId");
-    const enableRichPresence = config.get<boolean>("enableRichPresence");
-    const sessionTimerFormat = config.get<string>("sessionTimerFormat");
+  const discordId = config.get<string>("updateDiscordId");
+  const enableRichPresence = config.get<boolean>("enableRichPresence");
+  const sessionTimerFormat = config.get<string>("sessionTimerFormat");
 
-    console.log(`<< Config - Discord ID: ${discordId}, Rich Presence: ${enableRichPresence}, Timer Format: ${sessionTimerFormat} >>`);
+  console.log(
+    `<< Config - Discord ID: ${discordId}, Rich Presence: ${enableRichPresence}, Timer Format: ${sessionTimerFormat} >>`
+  );
 
-    if (discordId) {
-        extensionContext.globalState.update("discordId", discordId);
-    }
+  if (discordId) {
+    extensionContext.globalState.update("discordId", discordId);
+  }
 }
 
 export async function deactivate() {
-    stopSessionTimer();
-    stopRichPresence();
+  stopSessionTimer();
+  stopRichPresence();
 
-    const duration = await endSession();
-    const discordId = extensionContext.globalState.get<string>("discordId");
-    const lastSessionDate = new Date().toISOString();
-    const languages = getLanguageDurations();
-    const streakData = getStreakData();
+  const duration = await endSession();
+  const discordId = extensionContext.globalState.get<string>("discordId");
+  let discordUsername: string | null = null;
+  if (discordId) { discordUsername = await getDiscordUsername(discordId); }
+  const lastSessionDate = new Date().toISOString();
+  const languages = getLanguageDurations();
+  const streakData = getStreakData();
 
-    console.log(`<< Discord User Id: ${discordId} >>`);
-    console.log(`<< Session duration: ${duration} seconds >>`);
-    console.log(`<< Last session date: ${lastSessionDate} >>`);
-    console.log(`<< Session languages: ${JSON.stringify(languages)} >>`);
-    console.log(`<< Streak Data: ${JSON.stringify(streakData)} >>`);
+  console.log(`<< Discord User Id: ${discordId} >>`);
+  console.log(`<< Discord Username: ${discordUsername} >>`);
+  console.log(`<< Session duration: ${duration} seconds >>`);
+  console.log(`<< Last session date: ${lastSessionDate} >>`);
+  console.log(`<< Session languages: ${JSON.stringify(languages)} >>`);
+  console.log(`<< Streak Data: ${JSON.stringify(streakData)} >>`);
 
-    if (!discordId || !duration) {
-        console.log("<< Error: Missing required data. Discord ID or Duration is null >>");
-        return;
-    }
+  if (!discordId || !duration) {
+    console.log(
+      "<< Error: Missing required data. Discord ID or Duration is null >>"
+    );
+    return;
+  }
+
+  try {
+
+    console.log("<< Sending session data to Discord... >>");
+    await sendSessionData(
+      discordId,
+      discordUsername ?? "",
+      duration,
+      lastSessionDate,
+      languages,
+      streakData
+    );
     
-    try {
-        console.log("<< Sending session data to Discord... >>");
-        await sendSessionData(discordId, duration, lastSessionDate, languages, streakData);
-        console.log("<< Session data sent successfully! >>");
-    } catch (error) {
-        console.error(`<< Failed to send session data: ${error} >>`);
-    }
+    console.log("<< Session data sent successfully! >>");
+  } catch (error) {
+    console.error(`<< Failed to send session data: ${error} >>`);
+  }
 
-    console.log("<< Extension Deactivated >>");
+  console.log("<< Extension Deactivated >>");
 }
 
 // Timer function with format setting
 function startSessionTimer() {
-    const config = vscode.workspace.getConfiguration("extension");
-    const sessionTimerFormat = config.get<string>("sessionTimerFormat");
+  const config = vscode.workspace.getConfiguration("extension");
+  const sessionTimerFormat = config.get<string>("sessionTimerFormat");
 
-    statusBarTimer.show();
-    sessionTimerInterval = setInterval(() => {
-        const elapsed = new Date().getTime() - (sessionStartTime?.getTime() ?? 0);
-        const hours = Math.floor((elapsed / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((elapsed / (1000 * 60)) % 60);
-        const seconds = Math.floor((elapsed / 1000) % 60);
+  statusBarTimer.show();
+  sessionTimerInterval = setInterval(() => {
+    const elapsed = new Date().getTime() - (sessionStartTime?.getTime() ?? 0);
+    const hours = Math.floor((elapsed / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((elapsed / (1000 * 60)) % 60);
+    const seconds = Math.floor((elapsed / 1000) % 60);
 
-        switch (sessionTimerFormat) {
-            case "hh:mm:ss":
-                statusBarTimer.text = `Session Timer: ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-                break;
-            case "mm:ss":
-                statusBarTimer.text = `Session Timer: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-                break;
-            case "hours":
-                statusBarTimer.text = `Session Duration: ${((elapsed / (1000 * 60 * 60))).toFixed(1)} hrs`;
-                break;
-            default:
-                statusBarTimer.text = `Session Timer: ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-        }
-    }, 1000);
+    switch (sessionTimerFormat) {
+      case "hh:mm:ss":
+        statusBarTimer.text = `Session Timer: ${hours
+          .toString()
+          .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
+          .toString()
+          .padStart(2, "0")}`;
+        break;
+      case "mm:ss":
+        statusBarTimer.text = `Session Timer: ${minutes
+          .toString()
+          .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+        break;
+      case "hours":
+        statusBarTimer.text = `Session Duration: ${(
+          elapsed /
+          (1000 * 60 * 60)
+        ).toFixed(1)} hrs`;
+        break;
+      default:
+        statusBarTimer.text = `Session Timer: ${hours
+          .toString()
+          .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
+          .toString()
+          .padStart(2, "0")}`;
+    }
+  }, 1000);
 }
 
 function stopSessionTimer() {
-    if (sessionTimerInterval) {
-        clearInterval(sessionTimerInterval);
-        sessionTimerInterval = null;
-    }
-    statusBarTimer.hide();
+  if (sessionTimerInterval) {
+    clearInterval(sessionTimerInterval);
+    sessionTimerInterval = null;
+  }
+  statusBarTimer.hide();
 }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,10 +4,11 @@ import * as vscode from "vscode";
 const endpointUrl = "";
 
 // Function to send session data
-export async function sendSessionData(userId: string, duration: number, sessionDate: string, languages: Record<string, number>, streakData: { currentStreak: number; longestStreak: number }) {
+export async function sendSessionData(userId: string, username: string, duration: number, sessionDate: string, languages: Record<string, number>, streakData: { currentStreak: number; longestStreak: number }) {
     try {
         const response = await axios.post(endpointUrl, {
             userId,
+            username,
             duration,
             sessionDate,
             languages,
@@ -19,30 +20,21 @@ export async function sendSessionData(userId: string, duration: number, sessionD
     }
 }
 
-// Check if a Discord user ID has a valid format (17-18 numeric digits)
+// Check if a Discord user ID has a valid format (15-32 numeric digits)
 function isValidDiscordId(userId: string): boolean {
     return /^\d{15,32}$/.test(userId);
 }
 
 // Validate the Discord user ID by checking both format and API verification
 export async function checkAndValidateUserId(userId: string): Promise<boolean> {
-    // Check if the ID format is valid
     if (!isValidDiscordId(userId)) {
         vscode.window.showErrorMessage("<< Invalid Discord ID format | Enable Developer Mode In Discord And Try Again >>");
         return false;
     }
 
-    // Check that the token is loaded
-    // if (!process.env.DISCORD_TOKEN) {
-    //     console.log("<< DISCORD_TOKEN:", process.env.DISCORD_TOKEN);
-    //     vscode.window.showErrorMessage("<< Discord token is not loaded. Please check your .env configuration. >>");
-    //     return false;
-    // }
-
-    // API call to verify if the ID exists on Discord
     try {
         const response = await axios.get(`https://discord.com/api/v10/users/${userId}`, {
-            headers: { Authorization: `Bot BOT_TOKEN` }, // Has to be a string
+            headers: { Authorization: `Bot BOT_TOKEN` },
         });
         
         // If the request is successful, ID exists
@@ -50,7 +42,7 @@ export async function checkAndValidateUserId(userId: string): Promise<boolean> {
             console.log("<< Discord ID is valid >>");
             return true;
         }
-    } catch (error: any) {  // 'any' type to access `response` properties
+    } catch (error: any) {
         const status = error.response?.status;
         if (status === 401) {
             vscode.window.showErrorMessage("<< Unauthorized: Check bot token permissions >>");
@@ -62,4 +54,32 @@ export async function checkAndValidateUserId(userId: string): Promise<boolean> {
         }
     }
     return false;
+}
+
+// Function to fetch the username from the Discord API
+export async function getDiscordUsername(userId: string): Promise<string | null> {
+    try {
+        if (userId === null) { return null;}
+
+        const response = await axios.get(`https://discord.com/api/v10/users/${userId}`, {
+            headers: { Authorization: `Bot BOT_TOKEN` },
+        });
+
+        if (response.status === 200) {
+            const username = response.data.username; // Assuming the API returns `username` in the response
+            console.log(`<< Fetched Discord username: ${username} >>`);
+            return username;
+        }
+    } catch (error: any) {
+        const status = error.response?.status;
+        if (status === 401) {
+            vscode.window.showErrorMessage("<< Unauthorized: Check bot token permissions >>");
+        } else if (status === 404) {
+            vscode.window.showErrorMessage("<< The Discord ID does not exist | Please enter a valid ID >>");
+        } else {
+            console.error("<< Error fetching Discord username:", error.response?.data || error.message);
+            vscode.window.showErrorMessage("<< Error connecting to Discord API | Please try again later >>");
+        }
+    }
+    return null;
 }
