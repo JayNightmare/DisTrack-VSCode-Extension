4297f9fc02673d4a55a6809fc063da920e285dd7
JayNightmare <jordanbell321@gmail.com>
2025-09-20 14:29:49 +0100

    Add ignoreFocusOut option to command input for better user experience

--- src/extension.ts
+++ src/extension.ts
@@ -262,6 +262,7 @@ export async function activate(context: vscode.ExtensionContext) {
                                 }
                                 return null;
                             },
+                            ignoreFocusOut: true,
                         });
 
                         console.log(
